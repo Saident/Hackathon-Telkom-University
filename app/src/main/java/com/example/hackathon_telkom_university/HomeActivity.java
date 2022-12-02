b12e31639341386a66bac9066daca7be8f1e555b
Saident <averilprimayuda@gmail.com>
2022-12-03 02:24:25 +0700

    update on login and register

--- app/src/main/java/com/example/hackathon_telkom_university/HomeActivity.java
+++ app/src/main/java/com/example/hackathon_telkom_university/HomeActivity.java
@@ -6,15 +6,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
+
+    protected TextView getuser;
+    protected String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        getuser = findViewById(R.id.getUser);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                getuser.setText(user.fullname);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
@@ -26,7 +54,6 @@ public class HomeActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 switch(item.getItemId())
                 {
                     case R.id.search:
--- app/src/main/java/com/example/hackathon_telkom_university/LoginActivity.java
+++ app/src/main/java/com/example/hackathon_telkom_university/LoginActivity.java
@@ -1,14 +1,124 @@
 package com.example.hackathon_telkom_university;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity{
+
+    protected FirebaseAuth mAuth;
+    protected FirebaseDatabase firebaseDatabase;
+
+    protected EditText username, password;
+    protected TextView forgot_password, register;
+    protected Button button_login;
+    protected ImageView google_login, facebook_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        //initialize
+        button_login = findViewById(R.id.button_login);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Loginuser();
+            }
+        });
+
+        forgot_password = findViewById(R.id.forgot_password);
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        google_login = findViewById(R.id.google_login);
+        google_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        facebook_login = findViewById(R.id.facebook_login);
+        facebook_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //end of initialize
+    }
+
+    private void Loginuser() {
+        String email = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if(email.isEmpty()){
+            username.setError("Email is required!");
+            username.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            username.setError("Please enter a valid email");
+            username.requestFocus();
+            return;
+        }
+        if(pass.isEmpty()){
+            password.setError("Password is required!");
+            password.requestFocus();
+            return;
+        }
+        if(pass.length() < 6){
+            password.setError("Password needs to be 6 characters or longer");
+            password.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT);
+                            User user = new User();
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Failed to login!", Toast.LENGTH_SHORT);
+                            return;
+                        }
+                    }
+                });
     }
 }
