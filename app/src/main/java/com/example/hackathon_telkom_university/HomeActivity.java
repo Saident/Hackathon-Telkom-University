c51632c08af846e34b63bf8e34400399318bf478
Saident <averilprimayuda@gmail.com>
2022-12-04 13:24:10 +0700

    update map

--- app/src/main/java/com/example/hackathon_telkom_university/HomeActivity.java
+++ app/src/main/java/com/example/hackathon_telkom_university/HomeActivity.java
@@ -15,11 +15,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.Plugin;
 
 public class HomeActivity extends AppCompatActivity {
 
     protected TextView getuser;
     protected String name;
+    protected MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,15 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+
+        mapView = findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri("mapbox://styles/saident/clb3e1zo4000b14p2ecp3tim4", new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+
+            }
+        });
+
 
 
         // Initialize and assign variable
