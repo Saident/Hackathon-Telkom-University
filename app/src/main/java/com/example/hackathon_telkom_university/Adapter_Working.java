78cff23f204270223df8f00908ac14157f167531
Saident <averilprimayuda@gmail.com>
2022-12-05 09:27:48 +0700

    search dummy data

--- app/src/main/java/com/example/hackathon_telkom_university/Adapter_Working.java
+++ app/src/main/java/com/example/hackathon_telkom_university/Adapter_Working.java
@@ -1,40 +1,50 @@
 package com.example.hackathon_telkom_university;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Adapter_Working extends RecyclerView.Adapter<Adapter_Working.MyView> {
-    private List<String> list;
+    private ArrayList<Class_Coffee> list;
+    private Context context;
 
     public class MyView extends RecyclerView.ViewHolder {
-        TextView textView;
+        TextView name;
+        ImageView images;
 
         public MyView(View view) {
             super(view);
-            textView = (TextView)view.findViewById(R.id.rec_name);
+            name = itemView.findViewById(R.id.rec_name);
+            images = itemView.findViewById(R.id.iv_bg_coffee);
         }
     }
 
-    public Adapter_Working(List<String> horizontalList) {
-        this.list = horizontalList;
+    public Adapter_Working(Context context, List<Class_Coffee> horizontalList) {
+        this.context = context;
+        this.list = (ArrayList<Class_Coffee>) horizontalList;
     }
 
     @Override
     public MyView onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
+        View itemView = LayoutInflater.from(context)
                 .inflate(R.layout.item_working, parent, false);
         return new MyView(itemView);
     }
 
     @Override
     public void onBindViewHolder(final MyView holder, final int position) {
-        holder.textView.setText(list.get(position));
+        Class_Coffee coffee = list.get(position);
+        holder.name.setText(coffee.getNameCoffee());
+        Glide.with(context).load(coffee.getImageref()).into(holder.images);
     }
 
     @Override
