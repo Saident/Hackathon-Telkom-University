8ea8dac95d401ddee7ae017156deb0da8c1ea9d6
Saident <averilprimayuda@gmail.com>
2022-12-08 17:48:58 +0700

    logout

--- app/src/main/java/com/example/hackathon_telkom_university/Third_EditProfileActivity.java
+++ app/src/main/java/com/example/hackathon_telkom_university/Third_EditProfileActivity.java
@@ -1,14 +1,63 @@
 package com.example.hackathon_telkom_university;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Third_EditProfileActivity extends AppCompatActivity {
+
+    EditText fullname, username, email, phone;
+    Button save;
+    Class_User user = new Class_User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        fullname = findViewById(R.id.fullname);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user = snapshot.getValue(Class_User.class);
+                fullname.setText(user.fullname);
+                username.setText(user.username);
+                email.setText(user.email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        save = findViewById(R.id.button_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase.getInstance().getReference("Users")
+                        .child(FirebaseAuth.getInstance()
+                        .getCurrentUser().getUid()).setValue(user);
+            }
+        });
     }
 }
